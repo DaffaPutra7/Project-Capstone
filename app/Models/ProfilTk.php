489b20e378e99a319c63fc0db495541a77ce684d
DaffaPutra7 <putradika3604@gmail.com>
2025-10-23 20:17:20 +0700

    mengisi model di profiltk

--- app/Models/ProfilTk.php
+++ app/Models/ProfilTk.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ProfilTk extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'profil_tk';
+
+    protected $primaryKey = 'id_profil';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nama_tk',
+        'visi',
+        'misi',
+        'tujuan',
+        'motto',
+    ];
 }
