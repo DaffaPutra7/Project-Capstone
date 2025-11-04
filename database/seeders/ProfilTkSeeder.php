d4a43424f43efdd753197bdc5b8a3416f046988d
DaffaPutra7 <putradika3604@gmail.com>
2025-11-04 13:02:08 +0700

    memperbaiki penulisan pada misi dan tujuan

--- database/seeders/ProfilTkSeeder.php
+++ database/seeders/ProfilTkSeeder.php
@@ -12,9 +12,8 @@ class ProfilTkSeeder extends Seeder
         ProfilTk::create([
             'nama_tk' => 'TK Aisyiyah Bustanul Athfal Banjareja',
             'visi' => 'Mewujudkan anak didik menjadi generasi yang berakhlakul karimah, aktif, kreatif, asik, dan bertanggung jawab.',
-            'misi' => '1. Mencintai Al Qur’an melalui kegiatan tahfidzul qur’an.<br>2. Pembiasaan 5S (senyum, salam, sapa, sopan, santun).<br>3. Melaksanakan pembelajaran yang unggul dan kompeten.<br>4. Memotivasi anak dalam kegiatan yang asik dan menyenangkan tanpa beban.<br>5. Melatih anak menjadi pribadi yang mandiri, percaya diri, dan bertanggung jawab.',
-            'tujuan' => '1. Mewujudkan generasi hafidz-hafidzah yang berakhlakul karimah.<br>2. Membentuk generasi yang aktif, kreatif, asik, dan menyenangkan.<br>3. Membentuk generasi yang berkepribadian serta tanggung jawab dalam menghadapi era globalisasi.',
-            'motto' => 'Membentuk generasi “BAKAT” berakhlakul karimah, aktif, kreatif, asik, dan tanggung jawab.'
+            'misi' => "Mencintai Al Qur’an melalui kegiatan tahfidzul qur’an.\nPembiasaan 5S (senyum, salam, sapa, sopan, santun).\nMelaksanakan pembelajaran yang unggul dan kompeten.\nMemotivasi anak dalam kegiatan yang asik dan menyenangkan tanpa beban.\nMelatih anak menjadi pribadi yang mandiri, percaya diri, dan bertanggung jawab.",
+            'tujuan' => "Mewujudkan generasi hafidz-hafidzah yang berakhlakul karimah.\nMembentuk generasi yang aktif, kreatif, asik, dan menyenangkan.\nMembentuk generasi yang berkepribadian serta tanggung jawab dalam menghadapi era globalisasi.",
         ]);
     }
 }
