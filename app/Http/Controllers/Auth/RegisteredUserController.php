4f0337dc2ef156e8e34dfe9bd5e8b95936f11af7
DaffaPutra7 <putradika3604@gmail.com>
2025-11-07 15:56:05 +0700

    melakukan perbaikan pada no hp

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -33,7 +33,7 @@ class RegisteredUserController extends Controller
             'nama_lengkap' => ['required', 'string', 'max:100'],
             'email'        => ['required', 'string', 'lowercase', 'email', 'max:100', 'unique:users,email'],
             'password'     => ['required', 'confirmed', Rules\Password::defaults()],
-            'no_hp'        => ['nullable', 'string', 'numeric', 'min:10', 'max:20'],
+            'no_hp'        => ['nullable', 'string', 'numeric', 'digits_between:11,13'],
             'alamat'       => ['nullable', 'string'],
         ]);
 
