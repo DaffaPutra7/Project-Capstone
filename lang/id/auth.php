ce1e28398fdff060402c8b67ea7d4486abe4e234
DaffaPutra7 <putradika3604@gmail.com>
2025-11-11 14:47:01 +0700

    membuat file baru untuk autentikasi

---
+++ lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines (Bahasa Indonesia)
+    |--------------------------------------------------------------------------
+    |
+    | Baris bahasa berikut digunakan selama proses autentikasi untuk berbagai
+    | pesan yang perlu kita tampilkan kepada pengguna. Anda bebas
+    | memodifikasi baris bahasa ini sesuai kebutuhan aplikasi Anda.
+    |
+    */
+
+    'failed'   => 'Email atau kata sandi yang Anda masukkan salah.', // <-- INI YANG ANDA CARI
+    'password' => 'Kata sandi yang diberikan salah.',
+    'throttle' => 'Terlalu banyak percobaan login. Silakan coba lagi dalam :seconds detik.',
+
+];
