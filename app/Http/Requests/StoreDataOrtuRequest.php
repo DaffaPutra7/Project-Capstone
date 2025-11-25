f8a0a868c14a51b89368fa77519463d4b06eb79b
DaffaPutra7 <putradika3604@gmail.com>
2025-11-25 14:11:56 +0700

    mengubah data pendidikan agar data pendidikan yang ada di form orang tua hanya bisa memilih berdasarkan yang sudah di daftarkan

--- app/Http/Requests/StoreDataOrtuRequest.php
+++ app/Http/Requests/StoreDataOrtuRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class StoreDataOrtuRequest extends FormRequest
 {
@@ -13,19 +14,21 @@ class StoreDataOrtuRequest extends FormRequest
 
     public function rules(): array
     {
+        $pendidikan = ['SD', 'SMP', 'SMA', 'S1', 'S2', 'S3', 'Tidak Sekolah'];
+
         return [
             // Data Ayah (disamakan dengan 'name' di HTML)
             'nama_ayah' => 'required|string|max:100',
             'tempat_lahir_ayah' => 'required|string|max:50',
             'tanggal_lahir_ayah' => 'required|date',
-            'pendidikan_ayah' => 'required|string|max:50',
+            'pendidikan_ayah' => ['required', Rule::in($pendidikan)],
             'pekerjaan_ayah' => 'required|string|max:100',
 
             // Data Ibu (disamakan dengan 'name' di HTML)
             'nama_ibu' => 'required|string|max:100', 
             'tempat_lahir_ibu' => 'required|string|max:50',
             'tanggal_lahir_ibu' => 'required|date',
-            'pendidikan_ibu' => 'required|string|max:50', 
+            'pendidikan_ibu' => ['required', Rule::in($pendidikan)],
             'pekerjaan_ibu' => 'required|string|max:100',
 
             // Data Wali
