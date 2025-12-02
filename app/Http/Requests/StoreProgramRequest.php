88c5ab5323358ac4ad2f24ed0a113496c54f05ea
DaffaPutra7 <putradika3604@gmail.com>
2025-12-02 15:03:39 +0700

    Menambahkan custom attributes dan pesan error validasi bahasa indonesia pada form program

--- app/Http/Requests/StoreProgramRequest.php
+++ app/Http/Requests/StoreProgramRequest.php
@@ -16,8 +16,30 @@ class StoreProgramRequest extends FormRequest
     {
         return [
             'no_hp' => 'required|string|min:10|max:20',
-
             'jenis_program' => ['required', Rule::in(['Reguler', 'Full Day'])],
+        ];
+    }
+
+    /**
+     * Ubah nama atribut database menjadi nama yang enak dibaca user.
+     */
+    public function attributes(): array
+    {
+        return [
+            'no_hp' => 'Nomor HP',
+            'jenis_program' => 'Jenis Program',
+        ];
+    }
+
+    /**
+     * Kustomisasi pesan error.
+     */
+    public function messages(): array
+    {
+        return [
+            'required' => 'Data :attribute wajib diisi.',
+            'min' => 'Data :attribute minimal :min karakter.',
+            'max' => 'Data :attribute maksimal :max karakter.',
         ];
     }
 }
