ea3f69261fc5f237d2b124724ead1c4122d863a2
DaffaPutra7 <putradika3604@gmail.com>
2025-12-08 13:54:03 +0700

    Memperbarui logika dashboard agar dapat memfilter data pendaftar berdasarkan pilihan tahun ajaran secara dinamis

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,13 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Pendaftaran;
 use App\Models\TahunAjaran;
 use Illuminate\Http\Request;
+use Carbon\Carbon; 
 
 class AdminController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        // Ambil tahun ajaran aktif (paling baru)
-        $tahunAktif = TahunAjaran::orderBy('tahun', 'desc')->first();
+        $semuaTahunDb = TahunAjaran::orderBy('tahun', 'desc')->get();
+
+        $tahunSekarang = Carbon::now()->year;
+        $opsiTahunBaru = [];
+        
+        for ($i = 0; $i < 4; $i++) {
+            $start = $tahunSekarang + $i;
+            $end = $start + 1;
+            $stringTahun = $start . '/' . $end;
+            $opsiTahunBaru[] = $stringTahun;
+        }
+
+        $gabunganTahun = $semuaTahunDb->pluck('tahun')->toArray();
+        $semuaOpsiTahun = array_unique(array_merge($opsiTahunBaru, $gabunganTahun));
+        rsort($semuaOpsiTahun); 
+
+        $tahunPilihan = $request->query('tahun');
+        
+        if ($tahunPilihan) {
+            $tahunAktif = TahunAjaran::where('tahun', $tahunPilihan)->first();
+        } else {
+            $tahunAktif = TahunAjaran::orderBy('tahun', 'desc')->first();
+        }
 
         $jumlahPendaftar = 0;
         $sisaKuota = 0; 
@@ -35,6 +57,8 @@ class AdminController extends Controller
         }
 
         return view('admin.dashboard', [
+            'semuaTahun' => $semuaTahunDb,
+            'semuaOpsiTahun' => $semuaOpsiTahun,
             'tahunAktif' => $tahunAktif,
             'jumlahPendaftar' => $jumlahPendaftar,
             'sisaKuota' => $sisaKuota, 
