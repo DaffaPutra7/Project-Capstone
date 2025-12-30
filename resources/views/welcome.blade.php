fbcad0478ea54cf9b1ce7b3f38e0983855e4836b
ijaamm <ijammusyaf@gmaiil.com>
2025-12-30 21:08:46 +0700

    u[date tittle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>PPDB-TK | TK Aisyiyah Bustanul Athfal Banjareja</title>
+  <title>SPMB-TK | TK Aisyiyah Bustanul Athfal Banjareja</title>
   <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
